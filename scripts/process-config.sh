be42d7e7287d619e0d4816973d03a70ab55cb0ac
Paulo Bueno <paulob.bruno@gmail.com>
2025-09-22 23:01:52 -0300

    Adding hass

--- scripts/process-config.sh
+++ scripts/process-config.sh
@@ -4,6 +4,8 @@ apk add --no-cache gettext
 
 # Process the template and create the configuration
 envsubst < /input/zigbee2mqtt/configuration.yaml > /output/zigbee2mqtt/configuration.yaml
+cp /input/zigbee2mqtt/*.db /output/zigbee2mqtt/
+cp /input/zigbee2mqtt/*.js /output/zigbee2mqtt/
 cp /input/mosquitto/mosquitto.conf /output/mosquitto/mosquitto.conf
 
 echo "Configuration file generated successfully"
